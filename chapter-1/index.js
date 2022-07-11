9dde8c975efa7eef777698a129c66edd3b147be5
knightch <amankumarpandey2397@gmail.com>
2022-07-11 11:41:27 +0530

    Application Modularization

---
+++ chapter-1/index.js
@@ -0,0 +1,6 @@
+const math = require('./math');
+
+console.log("addition of two numbers  "+math.add(40,45));
+console.log("subtraction of two numbers  "+math.sub(40,45));
+console.log("division of two numbers  "+math.div(40,45));
+console.log("multipication of two numbers  "+math.multi(40,45));
---
+++ chapter-1/math.js
@@ -0,0 +1,24 @@
+// addition
+function add(a, b) {
+    return a + b;
+}
+// subtraction
+function sub(a, b) {
+    return a - b;
+}
+// multipication
+function multi(a, b) {
+    return a * b;
+}
+// division
+function div(a, b) {
+    return a % b;
+}
+
+// export module
+module.exports = {
+    add,
+    sub,
+    multi,
+    div
+};
